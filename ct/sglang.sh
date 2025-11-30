8bd4fd596c7f0e96d8650eb473bf935506cac889
Ian Knox <ian@ianknox.net>
2025-11-30 16:54:42 -0600

    added ct

---
+++ ct/sglang.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://git.community-scripts.org/community-scripts/ProxmoxVED/raw/branch/main/misc/build.func)
+# Author: Ian Knox (iknox)
+# License: MIT
+# Source:
+
+APP="SGLang"
+var_tags="${var_tags:-}"
+var_cpu="${var_cpu:-4}"
+var_ram="${var_ram:-8192}"
+var_disk="${var_disk:-20}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+#var_fuse="${var_fuse:-no}"
+#var_tun="${var_tun:-no}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+  if [[ ! -d /var ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  msg_info "Updating $APP LXC"
+  $STD apt update
+  $STD apt upgrade -y
+  msg_ok "Updated $APP LXC"
+  cleanup_lxc
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!"
+msg_custom "🚀" "${GN}" "${APP} setup has been successfully initialized!"
